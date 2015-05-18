b9fe3cfecbd08a64321ab6b2c980206654aa8364
IljaGoushcha <iljagou@yahoo.com>
2015-05-18 00:19:45 -0400

    played with smtp settings, added templetaes or views and received my first e-mail

--- profile_page_api/config/environments/development.rb
+++ profile_page_api/config/environments/development.rb
@@ -17,12 +17,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # adding per rails cast
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "mail.google.com",
-    authentication: "plain",
+    domain: "gmail.com",
+    authentication: :login,
     enable_starttls_auto: true,
     user_name: ENV["gmail_username"],
     password: ENV["gmail_password"]

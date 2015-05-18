c46b72958f6114a95fcf8f848770f89584bcbc16
IljaGoushcha <iljagou@yahoo.com>
2015-05-18 01:10:56 -0400

    continue to ploish send email functionality

--- profile_page_api/app/controllers/visitors_controller.rb
+++ profile_page_api/app/controllers/visitors_controller.rb
@@ -3,8 +3,7 @@ class VisitorsController < ApplicationController
   skip_before_action :verify_authenticity_token #added to avoid 422 error
 
   def send_email
-    binding.pry
-    VisitorMailer.message_from_visitor(params[:body]).deliver_now
+    VisitorMailer.message_from_visitor(params).deliver_now
   end
 
 end
--- profile_page_api/app/mailers/visitor_mailer.rb
+++ profile_page_api/app/mailers/visitor_mailer.rb
@@ -1,9 +1,11 @@
 class VisitorMailer < ActionMailer::Base
   default from: "iljagou@gmail.com" # this should not be a default in my case, rather optional e-mail from user
 
-  def message_from_visitor(body)
-    @body = body
+  def message_from_visitor(visitor)
 
-    mail to: "iljagou@yahoo.com", subject: "message from profile page visitor"
+    @name = visitor[:name]
+    @body = visitor[:body]
+
+    mail to: "iljagou@yahoo.com", subject: "message from profile page visitor (#{@name})"
   end
 end

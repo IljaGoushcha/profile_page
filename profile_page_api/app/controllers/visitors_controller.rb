3659a1fd2aa914933cd4033108f62d26f5479477
IljaGoushcha <iljagou@yahoo.com>
2015-05-17 20:17:35 -0400

    added routes and visitors_controller

---
+++ profile_page_api/app/controllers/visitors_controller.rb
@@ -0,0 +1,7 @@
+class VisitorsController < ApplicationController
+
+  def send_email
+    binding.pry
+  end
+
+end
--- profile_page_api/app/mailers/visitor_mailer.rb
+++ profile_page_api/app/mailers/visitor_mailer.rb
@@ -1,9 +1,9 @@
 class VisitorMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "from@example.com" # this should not be a default in my case, rather optional e-mail from user
 
   def message_from_visitor
     @greeting = "Hi"
 
-    mail to: "to@example.org", subject: "message from profile page visitor"
+    mail to: "iljagou@yahoo.com", subject: "message from profile page visitor"
   end
 end

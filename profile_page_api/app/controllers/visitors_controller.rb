6369217b0acff3572aa71b07860d09dfd6e70551
IljaGoushcha <iljagou@yahoo.com>
2015-05-18 00:35:35 -0400

    polishing my e-mail sending functionality

--- profile_page_api/app/controllers/visitors_controller.rb
+++ profile_page_api/app/controllers/visitors_controller.rb
@@ -3,7 +3,8 @@ class VisitorsController < ApplicationController
   skip_before_action :verify_authenticity_token #added to avoid 422 error
 
   def send_email
-    VisitorMailer.message_from_visitor.deliver_now
+    binding.pry
+    VisitorMailer.message_from_visitor(params[:body]).deliver_now
   end
 
 end
--- profile_page_api/app/mailers/visitor_mailer.rb
+++ profile_page_api/app/mailers/visitor_mailer.rb
@@ -1,8 +1,8 @@
 class VisitorMailer < ActionMailer::Base
   default from: "iljagou@gmail.com" # this should not be a default in my case, rather optional e-mail from user
 
-  def message_from_visitor
-    @greeting = "Hi"
+  def message_from_visitor(body)
+    @body = body
 
     mail to: "iljagou@yahoo.com", subject: "message from profile page visitor"
   end
